4e2dabff8f216dd75ee6edc2600ad4ded4009cf7
Niall McKinney <nmckinney@gmail.com>
2025-11-11 02:52:01 +0000

    fix: update Last Player Standing test to include HeaderComponent

--- apps/last-player-standing/src/app/app.spec.ts
+++ apps/last-player-standing/src/app/app.spec.ts
@@ -1,22 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
-import { NxWelcome } from './nx-welcome';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './shared/components/header.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterModule.forRoot([])],
-      declarations: [App, NxWelcome],
+      declarations: [App, HeaderComponent],
+      schemas: [NO_ERRORS_SCHEMA] // Ignore unknown elements in test
     }).compileComponents();
   });
 
-  it('should render title', () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome last-player-standing'
-    );
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should have last-player-standing title', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('last-player-standing');
   });
 });
